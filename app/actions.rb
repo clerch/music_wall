299f0e653bd332a7e32f13e3375bf16693d6ec9a
clerch <lerchc@gmail.com>
2016-05-16 13:13:06 -0700

    initial commit

---
+++ app/actions.rb
@@ -0,0 +1,58 @@
+# Homepage (Root path)
+get '/' do
+  erb :index
+end
+
+get '/tracks' do 
+  @tracks = Track.all 
+  ##this allows for us to have access to @messages in the index.erb file.
+  erb :'tracks/index'
+end
+
+post '/tracks' do
+  @track = Track.new(
+    url: params[:url],
+    song_title: params[:song_title],
+    author: params[:author]
+    )
+  if @track.save
+    redirect '/tracks'
+  else
+    erb :'tracks/new'
+  end
+end
+
+get '/tracks/new' do 
+  @track = Track.new
+  erb :'tracks/new'
+end
+
+get '/tracks' do 
+  @track = Track.find params[:id]
+  erb :'tracks/show'
+end
+
+
+# /messages?id=4567890
+
+
+get '/tracks/:id' do 
+  @track = Track.find params[:id]
+  erb :'tracks/show'
+end
+
+# /messages/4567890
+
+
+
+# delete '/messages/:id' do
+#   active record delete method params[:id]
+
+# patch
+
+# put '/messages/:id' do
+#   active record edit method 
+
+
+# get '/messages/:id?action=delete' do
+#   active record delete method params[:id]
---
+++ app/models/track.rb
@@ -0,0 +1,9 @@
+class Track < ActiveRecord::Base
+
+  validates :song_title, presence: true
+  validates :author, presence: true
+  validates :url, format: { with: /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/, message: "a valid url is required" }
+
+
+
+end
